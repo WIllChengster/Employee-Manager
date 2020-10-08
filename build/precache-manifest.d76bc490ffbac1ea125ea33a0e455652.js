7e93157b1bfb12ca8dd6835774f1664005784049
William Cheng <willchengsters@gmail.com>
2020-10-07 19:30:29 -0700

    bundle

---
+++ build/precache-manifest.d76bc490ffbac1ea125ea33a0e455652.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "839a64a4db44ec17b3fe93902dc1a656",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bb0370d27c167b6c5e0",
+    "url": "/static/js/2.c1a8c1d1.chunk.js"
+  },
+  {
+    "revision": "e4b6e979ff9c88eaf985c0fc87e58ea1",
+    "url": "/static/js/2.c1a8c1d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "034884e0ec4eb08606b7",
+    "url": "/static/js/main.7360c486.chunk.js"
+  },
+  {
+    "revision": "60190107a9fb73db5e4b",
+    "url": "/static/js/runtime-main.adabd2cd.js"
+  },
+  {
+    "revision": "f568b3f7fba50c97039cdc088b6ef77a",
+    "url": "/static/media/transparent-logo.f568b3f7.png"
+  }
+]);
